13d0e1a3a3cee16b5b7bab887b5894a26cf7f520
Google Google <Google@students.softuni.bg>
2019-03-07 22:04:05 +0200

    Added Software Engineering courses

---
+++ Portfolio/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Courses()
+        {
+            return this.View();
+        }
+    }
+}
